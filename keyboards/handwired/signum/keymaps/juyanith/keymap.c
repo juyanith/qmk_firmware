825a8b1630add0daf1ce8bc9e94b90d522186f14
Van Stonecypher <van.stonecypher@quantitech.com>
2018-06-14 15:35:41 -0500

    Renaming layers.

--- keyboards/handwired/signum/keymaps/juyanith/keymap.c
+++ keyboards/handwired/signum/keymaps/juyanith/keymap.c
@@ -1,6 +1,7 @@
 #include "signum.h"
 
 #define _C_DEL MT(MOD_LCTL, KC_DEL)
+#define _S_DEL MT(MOD_LSFT, KC_DEL)
 #define _S_BKSP MT(MOD_LSFT, KC_BSPC)
 #define _L_SPC LT(_RAISE, KC_SPC)
 #define _R_ENT LT(_LOWER, KC_ENT)
@@ -17,8 +18,12 @@
 #define _PASTE LCTL(KC_V)
 #define _G_TAB RGUI(KC_TAB)
 
+#define TO__BASE TO(_BASE)
+#define TO_LOWR TO(_LOWER)
+#define TO_RAIS TO(_RAISE)
+
 enum signum_layers {
-	_MAIN,
+	_BASE,
 	_LOWER,
 	_RAISE,
 	_ADJUST
@@ -30,17 +35,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL, 
 		KC_LBRC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, 
 		KC_RBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
-		TO(1),   KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BKSP,      _L_SPC,  _R_ENT,  KC_BSPC, KC_DEL,  KC_APP,  TO(2)  ),
-
-	KEYMAP(
-		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
-		KC_PSCR, _S_TAB,  _SC_TAB, _C_TAB,  KC_TAB,  _C_UP,        _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT, 
-		KC_INS,  _UNDO,   _CUT,    _COPY,   _PASTE,  _C_DOWN,      _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PAUS, 
-		TO(0),   _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______),
+		TO_LOWR, KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BKSP,      _L_SPC,  _R_ENT,  KC_BSPC, KC_DEL,  KC_APP,  TO_RAIS),
 
 	KEYMAP(
 		KC_DOT,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, 
 		KC_LBRC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, 
 		KC_RBRC, KC_TILD, KC_GRV,  KC_MINS, KC_PLUS, KC_UNDS,      KC_LCBR, KC_RCBR, KC_LABK, KC_RABK, KC_QUES, KC_PIPE, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, TO(0)  )
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, TO__BASE),
+
+	KEYMAP(
+		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
+		KC_PSCR, _S_TAB,  _SC_TAB, _C_TAB,  KC_TAB,  _C_UP,        _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT, 
+		KC_INS,  _UNDO,   _CUT,    _COPY,   _PASTE,  _C_DOWN,      _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PAUS, 
+		TO__BASE, _______, _______, _______, _______, _S_DEL,      _______, _______, _______, _______, _______, _______),
+
+	KEYMAP(
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______)
+
 };
