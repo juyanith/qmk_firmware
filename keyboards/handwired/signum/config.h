b9ca5c4266744c02197a87d90f1b21401c3bacad
Van Stonecypher <van.stonecypher@quantitech.com>
2018-06-14 07:22:16 -0500

    Fixed pin assignments.

--- keyboards/handwired/signum/config.h
+++ keyboards/handwired/signum/config.h
@@ -25,26 +25,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x6060
 #define MANUFACTURER    TroyFletcher
 #define PRODUCT         Signum
-#define DESCRIPTION     A 40% ergonomic keyboard
+#define DESCRIPTION     40% ergonomic keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
 
-/* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { D0, D5, B5, B6 }
-#define MATRIX_COL_PINS { F1, F0, B0, C7, F4, F5, F6, F7, D4, D6, B4, D7 }
+/* Signum PCB default pin-out */
+#define MATRIX_ROW_PINS { B3, F5, D2, B5 }
+#define MATRIX_COL_PINS { B2, B7, B1, D0, B0, D1, B6, F0, F7, F1, F6, F4 }
 #define UNUSED_PINS
-
-#define QMK_ESC_OUTPUT F1
-#define QMK_ESC_INPUT D5
-#define QMK_LED     E6
-#define QMK_SPEAKER C6
-
-#define AUDIO_VOICES
-#define C6_AUDIO
-
-#define BACKLIGHT_PIN B7
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -52,14 +42,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
-/* number of backlight levels */
+#ifdef BACKLIGHT_PIN
 #define BACKLIGHT_LEVELS 3
+#endif
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
+
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
@@ -67,6 +59,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
+
+#define PREVENT_STUCK_MODIFIERS
 
 /*
  * Feature disable options
