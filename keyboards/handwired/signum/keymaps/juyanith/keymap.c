993ed1e09add0e0b8be0c5a5ff8db26f24e5428b
Van Stonecypher <van.stonecypher@quantitech.com>
2018-06-15 12:29:54 -0500

    Attempt to rework layers.

--- keyboards/handwired/signum/keymaps/juyanith/keymap.c
+++ keyboards/handwired/signum/keymaps/juyanith/keymap.c
@@ -1,9 +1,11 @@
 #include "signum.h"
 
+#define _C_A_D LALT(LCTL(KC_DEL))
 #define _C_DEL CTL_T(KC_DEL)
-#define _S_DEL SFT_T(KC_DEL)
+#define _C_BSPC CTL_T(KC_BSPC)
 #define _S_BSPC SFT_T(KC_BSPC)
-#define _LTENT LT(_RAISE, KC_ENT)
+#define _ADJBSP LT(_ADJUST, KC_DEL)
+#define _ADJDEL LT(_ADJUST, KC_DEL)
 #define _S_TAB RSFT(KC_TAB)
 #define _SC_TAB RSFT(RCTL(KC_TAB))
 #define _C_TAB RCTL(KC_TAB)
@@ -17,9 +19,8 @@
 #define _PASTE LCTL(KC_V)
 #define _G_TAB RGUI(KC_TAB)
 
-#define TO_BASE TO(_BASE)
-#define TO_LOWR TO(_LOWER)
-#define TO_RAIS TO(_RAISE)
+#define TO_LOWR TO(_SYMBOL)
+#define TO_RAIS TO(_NAVIGATION)
 
 #define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
@@ -28,100 +29,87 @@
 #endif
 
 enum signum_keycodes {
-  _BKSDEL  = SAFE_RANGE, 
-  _SPCRTN,
-  _L_SPEN
+  M_SYMBL = SAFE_RANGE, 
+  M_NAV
 };
 
 enum signum_layers {
 	_BASE,
-	_LOWER,
-	_RAISE,
+	_SYMBOL,
+	_NAVIGATION,
 	_ADJUST
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	KEYMAP(
+	[_BASE] = KEYMAP(
 		KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL, 
 		KC_LBRC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, 
 		KC_RBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
-		KC_SPC, KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BSPC,       _L_SPEN, _LTENT,  KC_BSPC, KC_DEL,  KC_APP,  _SPCRTN),
+		_______, KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BSPC,      M_SYMBL, M_NAV,   KC_BSPC, KC_DEL,  KC_APP,  _______),
 
-	KEYMAP(
+	[_SYMBOL] = KEYMAP(
 		KC_DOT,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, 
 		KC_LBRC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, 
 		KC_RBRC, KC_TILD, KC_GRV,  KC_MINS, KC_PLUS, KC_UNDS,      KC_LCBR, KC_RCBR, KC_LABK, KC_RABK, KC_QUES, KC_PIPE, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, TO_BASE),
+		_______, _______, _______, _______, _C_BSPC, _ADJDEL,      _______, _______, _______, _______, _______, _______),
 
-	KEYMAP(
+	[_NAVIGATION] = KEYMAP(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
 		KC_PSCR, _S_TAB,  _SC_TAB, _C_TAB,  KC_TAB,  _C_UP,        _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT, 
 		KC_INS,  _UNDO,   _CUT,    _COPY,   _PASTE,  _C_DOWN,      _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PAUS, 
-		TO_BASE, _______, _______, _______, _______, _S_DEL,      _______, _______, _______, _______, _______, _______),
+		_______, _______, _______, _______, _C_DEL,  _ADJBSP,      _______, _______, _______, _______, _______, _______),
 
-	KEYMAP(
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______)
+	[_ADJUST] = KEYMAP(
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _C_A_D,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX)
 
 };
 
-static bool shift_interrupted = false;
+static bool layer_interrupted[] = { false, false, false, false };
+static uint16_t layer_timers[] = { 0, 0, 0, 0 };
+static bool shift_removed = false;
 
-void layer_shift_tap(int layer, uint16_t keycode, uint16_t kc_shifted, keyrecord_t *record) {
-	static uint16_t shift_timer = 0;
-	
+void layer_mod_tap(int layer,uint16_t keycode, uint16_t kc_shifted, keyrecord_t *record) {
 	if (record->event.pressed) {
-		shift_interrupted = false;
-		shift_timer = timer_read();
+		layer_interrupted[layer] = false;
+		layer_timers[layer] = timer_read();
 		layer_on(layer);
+		shift_removed = keyboard_report->mods & MOD_BIT(KC_LSHIFT);
+		if (shift_removed) del_mods(MOD_LSFT);
 	} else {
 		layer_off(layer);
-		if (!shift_interrupted && timer_elapsed(shift_timer) < TAPPING_TERM) {
-			if (keyboard_report->mods & MODS_SHIFT_MASK) {
+		if (shift_removed) add_mods(MOD_LSFT);
+		if (!layer_interrupted[layer] && timer_elapsed(layer_timers[layer]) < TAPPING_TERM) {
+			if (shift_removed) {
 				register_code (kc_shifted);
 				unregister_code(kc_shifted);
-			} else {
+			} else { 
 				register_code (keycode);
 				unregister_code(keycode);
 			}
-			shift_interrupted = true;
+			layer_interrupted[layer] = true;
 		}
 	}
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	static bool is_spc_modified = false;
-	
-	switch (keycode) {
-		case _BKSDEL: // Delete when shifted, othewise Backspace
-			keycode = keyboard_report->mods & MODS_SHIFT_MASK ? KC_DEL : KC_BSPC;
-			if (record->event.pressed) {
-				register_code(keycode);
-			} else {
-				unregister_code(keycode);
-			}
-			return false;
-			
-		case _SPCRTN: // Return when shifted, othewise Space
-			if (record->event.pressed) {
-				is_spc_modified = keyboard_report->mods & MODS_SHIFT_MASK;
-				keycode = is_spc_modified ? KC_ENT : KC_SPC;
-				register_code (keycode);
-			} else {
-				keycode = is_spc_modified ? KC_ENT : KC_SPC;
-				unregister_code (keycode);
-			}
+	switch (keycode) { 
+		case M_NAV: 
+			layer_mod_tap(_NAVIGATION, KC_ENT, KC_SPC, record);
 			return false;
 
-		case _L_SPEN:
-			layer_shift_tap(_LOWER, KC_SPC, KC_ENTER, record);
+		case M_SYMBL: 
+			layer_mod_tap(_SYMBOL, KC_SPC, KC_ENT, record);
 			return false;
-	  
+
 		default:
-			shift_interrupted = true;
+			layer_interrupted[_ADJUST] = true;
+			layer_interrupted[_BASE] = true;
+			layer_interrupted[_NAVIGATION] = true;
+			layer_interrupted[_SYMBOL] = true;
 			return true; // Process all other keycodes normally
 	}
 }
