e57a2eb2afb998e522943a6e4575f628448889e5
van.stonecypher <van@unnamed.net>
2018-06-14 22:27:51 -0500

    Added layer/shift/enter macro.

--- keyboards/handwired/signum/keymaps/juyanith/keymap.c
+++ keyboards/handwired/signum/keymaps/juyanith/keymap.c
@@ -1,10 +1,9 @@
 #include "signum.h"
 
-#define _C_DEL MT(MOD_LCTL, KC_DEL)
-#define _S_DEL MT(MOD_LSFT, KC_DEL)
-#define _S_BKSP MT(MOD_LSFT, KC_BSPC)
-#define _L_SPC LT(_RAISE, KC_SPC)
-#define _R_ENT LT(_LOWER, KC_ENT)
+#define _C_DEL CTL_T(KC_DEL)
+#define _S_DEL SFT_T(KC_DEL)
+#define _S_BSPC SFT_T(KC_BSPC)
+#define _LTENT LT(_RAISE, KC_ENT)
 #define _S_TAB RSFT(KC_TAB)
 #define _SC_TAB RSFT(RCTL(KC_TAB))
 #define _C_TAB RCTL(KC_TAB)
@@ -18,9 +17,21 @@
 #define _PASTE LCTL(KC_V)
 #define _G_TAB RGUI(KC_TAB)
 
-#define TO__BASE TO(_BASE)
+#define TO_BASE TO(_BASE)
 #define TO_LOWR TO(_LOWER)
 #define TO_RAIS TO(_RAISE)
+
+#define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
+#ifndef TAPPING_TERM
+#define TAPPING_TERM 200
+#endif
+
+enum signum_keycodes {
+  _BKSDEL  = SAFE_RANGE, 
+  _SPCRTN,
+  _LTSPRT
+};
 
 enum signum_layers {
 	_BASE,
@@ -35,19 +46,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL, 
 		KC_LBRC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,         KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, 
 		KC_RBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
-		TO_LOWR, KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BKSP,      _L_SPC,  _R_ENT,  KC_BSPC, KC_DEL,  KC_APP,  TO_RAIS),
+		KC_SPC, KC_ESC,  KC_LGUI, KC_LALT, _C_DEL,  _S_BSPC,      _LTSPRT, _LTENT,  KC_BSPC, KC_DEL,  KC_APP,  _SPCRTN),
 
 	KEYMAP(
 		KC_DOT,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, 
 		KC_LBRC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQUO, 
 		KC_RBRC, KC_TILD, KC_GRV,  KC_MINS, KC_PLUS, KC_UNDS,      KC_LCBR, KC_RCBR, KC_LABK, KC_RABK, KC_QUES, KC_PIPE, 
-		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, TO__BASE),
+		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, TO_BASE),
 
 	KEYMAP(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
 		KC_PSCR, _S_TAB,  _SC_TAB, _C_TAB,  KC_TAB,  _C_UP,        _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT, 
 		KC_INS,  _UNDO,   _CUT,    _COPY,   _PASTE,  _C_DOWN,      _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PAUS, 
-		TO__BASE, _______, _______, _______, _______, _S_DEL,      _______, _______, _______, _______, _______, _______),
+		TO_BASE, _______, _______, _______, _______, _S_DEL,      _______, _______, _______, _______, _______, _______),
 
 	KEYMAP(
 		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, 
@@ -56,3 +67,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______)
 
 };
+
+void LT_Shift(int layer, uint16_t keycode, uint16_t kc_shifted, keyrecord_t *record) {
+	static uint16_t timer = 0;
+	
+	if (record->event.pressed) {
+		timer = timer_read();
+		layer_on(_LOWER);
+	} else {
+		layer_off(_LOWER);
+		if (timer_elapsed(timer) < TAPPING_TERM) {
+			if (keyboard_report->mods & MODS_SHIFT_MASK) {
+				register_code (kc_shifted);
+				unregister_code(kc_shifted);
+			} else {
+				register_code (keycode);
+				unregister_code(keycode);
+			}
+		}
+	}
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	static bool is_spc_modified = false;
+	
+	switch (keycode) {
+		case _BKSDEL: // Delete when shifted, othewise Backspace
+			keycode = keyboard_report->mods & MODS_SHIFT_MASK ? KC_DEL : KC_BSPC;
+			if (record->event.pressed) {
+				register_code(keycode);
+			} else {
+				unregister_code(keycode);
+			}
+			return false;
+			
+		case _SPCRTN: // Return when shifted, othewise Space
+			if (record->event.pressed) {
+				is_spc_modified = keyboard_report->mods & MODS_SHIFT_MASK;
+				keycode = is_spc_modified ? KC_ENT : KC_SPC;
+				register_code (keycode);
+			} else {
+				keycode = is_spc_modified ? KC_ENT : KC_SPC;
+				unregister_code (keycode);
+			}
+			return false;
+
+		case _LTSPRT:
+			LT_Shift(_LOWER, KC_SPC, KC_ENTER, record);
+			return false;
+	  
+		default:
+			return true; // Process all other keycodes normally
+	}
+}
