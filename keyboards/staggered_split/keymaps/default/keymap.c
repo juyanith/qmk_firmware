3858c7d2701bf783610cc0c3e85a5be505faddb3
Van Stonecypher <van.stonecypher@quantitech.com>
2018-07-12 08:05:49 -0500

    Fixed keyboard layout.

--- keyboards/staggered_split/keymaps/default/keymap.c
+++ keyboards/staggered_split/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                              KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL,
 		KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                              KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
 		KC_UNDS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                              KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-		                  KC_PSCR, KC_LGUI, KC_LALT, _C_DEL,  _S_BSPC, KC_ESC,        KC_ENT,  _L_SYM,  _L_NAV,  KC_BSPC, KC_APP,  KC_INS),
+		                  KC_LGUI, KC_LALT, _C_DEL,  _S_BSPC, KC_BSPC, KC_ESC,        KC_BSPC, KC_ENT,  _L_SYM,  _L_NAV,  KC_APP, KC_INS),
 
 	[_QUERTY] = LAYOUT_ortho_5x12(
     _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ADJUST] = LAYOUT_ortho_5x12(
     _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
 		XXXXXXX, _T_QWRT, XXXXXXX, XXXXXXX, XXXXXXX, _D_STA1,                                           _D_STA2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _D_PLA1,                                           _D_PLA2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		KC_INS,  RESET,   XXXXXXX, XXXXXXX, XXXXXXX, _D_PLA1,                                           _D_PLA2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _D_STOP,                                           _D_STOP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		                  XXXXXXX, XXXXXXX, XXXXXXX, _C_S_E,  _______, XXXXXXX,       XXXXXXX, _______, _C_A_D,  XXXXXXX, XXXXXXX, XXXXXXX)
+		                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _C_S_E,        _C_A_D,  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX)
 
 };
 
