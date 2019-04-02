0b091155a33365f9436c74f87f050994a1466595
Van Stonecypher <van.stonecypher@quantitech.com>
2019-04-02 07:10:12 -0500

    Updated thumbkeys.

--- keyboards/xd75/keymaps/juyanith/keymap.c
+++ keyboards/xd75/keymaps/juyanith/keymap.c
@@ -15,7 +15,7 @@
  */
 #include "xd75.h"
 
-#define _ADJBS1 LT(_ADJUST, KC_BSPC)
+#define _ADJBS1 LT(_ADJUST, KC_DEL)
 #define _ADJBS2 LT(_ADJUST, KC_ENT)
 
 #define _C_A_D LALT(LCTL(KC_DEL))
@@ -35,8 +35,8 @@
 
 #define _G_TAB RGUI(KC_TAB)
 
-#define _L_NAV TT(_NAVIGATION)
-#define _L_SYM1 LT(_SYMBOL, KC_BSPC)
+#define _L_NAV LT(_NAVIGATION, KC_SPC)
+#define _L_SYM1 LT(_SYMBOL, KC_DEL)
 #define _L_SYM2 LT(_SYMBOL, KC_ENT)
 
 #define _S_BSPC SFT_T(KC_BSPC)
@@ -61,7 +61,6 @@ enum {
 	KILLCLR,
 	PASTE,
   SAVE,
-  SHIFT,
 	UNDO,
 	YANK,
 	DYNAMIC_MACRO_RANGE
@@ -87,20 +86,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | F      | P      | G      | HOME   | PRTSCR | END    | J      | L      | U      | Y      | ;      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | `      | A      | R      | S      | T      | D      | PG UP  | UP     | PG DN  | H      | N      | E      | I      | O      | '      |
+ * | BACKSP | A      | R      | S      | T      | D      | PG UP  | UP     | PG DN  | H      | N      | E      | I      | O      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | _      | Z      | X      | C      | V      | B      | LEFT   | DOWN   | RIGHT  | K      | M      | ,      | .      | /      | \      |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | CTRL   | OS     | ALT    | CTRL   | SHIFT  | BACKSP | DELETE | CAPS   | SHIFT  | ENTER  | SPACE  | FN     | MENU   | OS     | CTRL   |
+ * | CTRL   | OS     | ALT    | CTRL   | SHIFT  | BACKSP | DELETE | CAPS   | SHIFT  | ENTER  | SPACE  | ALTGR  | MENU   | OS     | CTRL   |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
 	[_COLEMAK] = LAYOUT(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC, KC_INS,  KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
 		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME, KC_PSCR, KC_END,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL, 
-		KC_GRV,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_PGUP, KC_UP,   KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, 
+    KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_PGUP, KC_UP,   KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
 		KC_UNDS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_DOWN, KC_RGHT, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, _SHIFT_, _L_SYM1, KC_DEL,  KC_CAPS, KC_RSFT, _L_SYM2, KC_SPC,  _L_NAV,  KC_APP,  KC_RGUI, KC_RCTL),
+		KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, _SHIFT_, _L_SYM1, KC_DEL,  KC_CAPS, KC_RSFT, _L_SYM2, _L_NAV,  KC_RALT, KC_APP,  KC_RGUI, KC_RCTL),
 
   /* Querty overlay
    * .--------------------------------------------------------------------------------------------------------------------------------------.
@@ -129,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
    * |        | 1      | 2      | 3      | 4      | 5      |        |        |        | 6      | 7      | 8      | 9      | 0      |        |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
-   * |        | !      | @      | #      | $      | %      |        |        |        | ^      | &      | *      | (      | )      |        |
+   * | DEL    | !      | @      | #      | $      | %      |        |        |        | ^      | &      | *      | (      | )      |        |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
    * |        | ~      | `      | _      | {      | [      |        |        |        | ]      | }      | -      | +      |        |        |
    * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
@@ -140,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_SYMBOL] = LAYOUT( 
 	  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     _______, KC_TILD, KC_GRV,  KC_UNDS, KC_LCBR, KC_LBRC, _______, _______, _______, KC_RBRC, KC_RCBR, KC_MINS, KC_PLUS, KC_EQL,  _______,
 		_______, _______, _______, _______, _______, _ADJBS1, _______, _______, _______, _ADJBS2, _______, _______, _______, _______, _______),
 
@@ -150,19 +149,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
    * |        | F1     | F2     | F3     | F4     | F5     | F6     |        | F7     | F8     | F9     | F10    | F11    | F12    |        |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
-   * | ERASE  | ALL    | KILL   | SAVE   | YANK   | SUP    |        |        |        | WLEFT  | LEFT   | DOWN   | UP     | RIGHT  | WRIGHT |
+   * |        | ALL    | KILL   | SAVE   | YANK   | SUP    |        |        |        | WLEFT  | LEFT   | DOWN   | UP     | RIGHT  | WRIGHT |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
-   * |        | UNDO   | CUT    | COPY   | PASTE  | SDOWN  |        |        |        |        |        |        |        |        |        |
+   * | ERASE  | UNDO   | CUT    | COPY   | PASTE  | SDOWN  |        |        |        |        |        |        |        |        |        |
    * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
    * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
    * '--------------------------------------------------------------------------------------------------------------------------------------'
    */
 
 	[_NAVIGATION] = LAYOUT(
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-    KILLCLR, ALL,     KILL,    SAVE,    YANK,    _C_UP,   _______, _______, _______, _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT,
-    _______, UNDO,    CUT,     COPY,    PASTE,   _C_DOWN, _______, _______, _______, _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+    _______, ALL,     KILL,    SAVE,    YANK,    _C_UP,   _______, _______, _______, _C_LEFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _C_RGHT,
+    KILLCLR, UNDO,    CUT,     COPY,    PASTE,   _C_DOWN, _______, _______, _______, _G_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
   /* Adjust
