a2ff6de7db4217f16881224fb3a24af3ebc0f51a
Van Stonecypher <van.stonecypher@quantitech.com>
2019-04-08 14:57:32 -0500

    Minor key rearrangement.

--- keyboards/staggered_split/keymaps/default/keymap.c
+++ keyboards/staggered_split/keymaps/default/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                              KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_EQL,
 		KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                              KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
 		KC_UNDS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                              KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-		                  KC_LGUI, KC_LALT, KC_LCTL, _SHIFT_, _L_SYM1, KC_DEL,        KC_DEL,  _L_SYM2, _L_NAV,  KC_RALT, KC_APP, KC_INS),
+		                  KC_LGUI, KC_LALT, KC_LCTL, _SHIFT_, _L_SYM1, KC_BSPC,       KC_DEL,  _L_SYM2, _L_NAV,  KC_RALT, KC_APP, KC_INS),
 
 	[_QUERTY] = LAYOUT_ortho_5x12(
     _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_SYMBOL] = LAYOUT_ortho_5x12(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+		KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
 		_______, KC_TILD, KC_GRV,  KC_UNDS, KC_LCBR, KC_LBRC,                                           KC_RBRC, KC_RCBR, KC_MINS, KC_PLUS, KC_EQL,  _______,
 		                  _______, _______, _______, _______, _ADJBS1, _______,       _______, _ADJBS2, _______, _______, _______, _______),
 
